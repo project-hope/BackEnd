0f6b1863f6ae9c93eeb6c786ba696855ba7c8fac
JonHalloran <angmar139@hotmail.com>
2019-03-14 12:40:30 -0700

    Let's hope this seeds file works

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,13 @@
 
 require 'csv'
 
-# csv_text_dis = File.read(Rails.root.join('lib', 'seeds', 'disabilityv2.csv'))
-# csv_dis = CSV.parse(csv_text_dis, :headers => true, :encoding => 'ISO-8859-1')
-# csv_dis.each do |row|
-#   new_row = row.to_hash
-#   new_row.delete(new_row.keys.first)
-#   disability = Disability.create!(new_row)
-# end
+csv_text_dis = File.read(Rails.root.join('lib', 'seeds', 'disabilityv2.csv'))
+csv_dis = CSV.parse(csv_text_dis, :headers => true, :encoding => 'ISO-8859-1')
+csv_dis.each do |row|
+  new_row = row.to_hash
+  new_row.delete(new_row.keys.first)
+  disability = Disability.create!(new_row)
+end
 
 csv_text_div = File.read(Rails.root.join('lib', 'seeds', 'divorcev2.csv'))
 csv_div = CSV.parse(csv_text_div, :headers => true, :encoding => 'ISO-8859-1')
@@ -23,5 +23,4 @@ csv_div.each do |row|
   new_row = row.to_hash
   new_row.delete(new_row.keys.first)
   divorce = Divorce.create!(new_row)
-  p divorce
 end
